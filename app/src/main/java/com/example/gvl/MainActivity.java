94801ec79bb8bb6f9ff410aee1552e014b0a334f
gringrant <gringrant>
2023-11-04 14:41:38 -0400

    Fixed merge

--- app/src/main/java/com/example/gvl/MainActivity.java
+++ app/src/main/java/com/example/gvl/MainActivity.java
@@ -5,16 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
-import android.app.Activity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -26,35 +18,15 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.Manifest;
-import android.widget.TextView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.gvl.ui.login.LoginActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Locale;
 import com.permissionx.guolindev.PermissionX;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     TextView currentLoc;
     Button getLocation;
 
-    private final static int REQUEST_CODE=100;
+    private final static int REQUEST_CODE = 100;
 
 
     @Override
@@ -70,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentLoc = findViewById(R.id.current_location);
+        currentLoc = findViewById(R.id.loc);
         getLocation = findViewById(R.id.locationFinder);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
@@ -98,17 +70,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 PermissionX.init(ma)
                         .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
-                        .request (
-                    (allGranted, grantedList, deniedList) ->
-                    {
-                        if (allGranted) {
-                            Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(ma, "Can't get precise location: permission denied", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                );
+                        .request(
+                                (allGranted, grantedList, deniedList) ->
+                                {
+                                    if (allGranted) {
+                                        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(ma, "Can't get precise location: permission denied", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                        );
 
                 /*Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(intent);*/
@@ -116,46 +88,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("MissingPermission")
     private void getLastLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>(){
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-                        List<Address> addresses = null;
-                        try {
-                            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            currentLoc.setText(addresses.get(0).getLocality()+", "+addresses.get(0).getAdminArea()+", "+addresses.get(0).getCountryName());
-                        } catch (IOException e) {
-                            e.printStackTrace();
+        AppCompatActivity ma = this;
+        PermissionX.init(ma)
+                .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .request(
+                        (allGranted, grantedList, deniedList) ->
+                        {
+                            if (allGranted) {
+                                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        if (location != null) {
+                                            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                            List<Address> addresses = null;
+                                            try {
+                                                addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                                currentLoc.setText(addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() /*+ ", " + addresses.get(0).getCountryName()*/);
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(ma, "Can't get precise location: permission denied", Toast.LENGTH_LONG).show();
+                            }
                         }
-                    }
-                }
-            });
-
-        }else{
-            askPermission();
-        }
+                );
 
     }
 
-    private void askPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]
-                {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
-    }
+    /*private void askPermission() {
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+}*/
 
-        if (requestCode==REQUEST_CODE){
-            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLastLocation();
-            } else {
-                Toast.makeText(this, "Required Permission", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 }
