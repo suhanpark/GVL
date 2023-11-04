d950df0dea35a7d905370db63a3ea053f6316170
gringrant <gringrant>
2023-11-04 13:33:23 -0400

    Added map

--- app/src/main/java/com/example/gvl/MainActivity.java
+++ app/src/main/java/com/example/gvl/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import android.Manifest;
 
 import com.example.gvl.ui.login.LoginActivity;
+import com.permissionx.guolindev.PermissionX;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +28,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        AppCompatActivity ma = this;
         ImageButton map_btn = (android.widget.ImageButton) findViewById(R.id.map_btn);
         map_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                startActivity(intent);
+                PermissionX.init(ma)
+                        .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .request (
+                    (allGranted, grantedList, deniedList) ->
+                    {
+                        if (allGranted) {
+                            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(ma, "Can't get precise location: permission denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                );
+
+                /*Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);*/
             }
         });
 
