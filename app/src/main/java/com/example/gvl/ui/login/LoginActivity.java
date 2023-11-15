2f1a4f280b4f30584d0648e619d037fad0989600
Henry Barton <hzb2019@yahoo.com>
2023-11-15 05:24:48 -0500

    added "remember me" checkbox and added functionality associated with it - saves username and password to preferences storage when checked and when login button is clicked cleaned up warning by adding string reference for activity_login.xml

--- app/src/main/java/com/example/gvl/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/gvl/ui/login/LoginActivity.java
@@ -7,7 +7,9 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -20,6 +22,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -36,6 +40,12 @@ public class LoginActivity extends AppCompatActivity {
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
 
+    // we use preferences to save the login info because it should not
+    // be accessible by other apps, data will be removed when the
+    // app is uninstalled, and we only need to use simple key:value pairs
+    // for the stored data
+    private SharedPreferences pref;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +56,12 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
+        pref = getSharedPreferences("login", Context.MODE_PRIVATE);
+
         final EditText usernameEditText = binding.username;
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
+        final CheckBox rememberMe = binding.remember;
         final ProgressBar loadingProgressBar = binding.loading;
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -87,6 +100,19 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // if remember preference is true, call loginDataChanged with the retrieved
+        // data to unblock the login button for convenience, then set the username
+        // and password text fields to the data that is saved to indicate data has
+        // been autofilled
+        if(pref.getBoolean("remember", false)) {
+            rememberMe.setChecked(true);
+            String username = pref.getString("username", "");
+            String password = pref.getString("password", "");
+            loginViewModel.loginDataChanged(username, password);
+            usernameEditText.setText(username);
+            passwordEditText.setText(password);
+        }
+
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -121,12 +147,40 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // if the "remember" field is checked, we want to write what is
+                // currently in the username and password fields to preferences
+                // if it is unchecked, we remove whatever is currently saved to
+                // those values for cleanup purposes
+                if(pref.getBoolean("remember", false)) {
+                    SharedPreferences.Editor prefEdit = pref.edit();
+                    prefEdit.putString("username", usernameEditText.getText().toString());
+                    prefEdit.putString("password", passwordEditText.getText().toString());
+                    prefEdit.apply();
+                } else {
+                    SharedPreferences.Editor prefEdit = pref.edit();
+                    prefEdit.remove("username");
+                    prefEdit.remove("password");
+                    prefEdit.apply();
+                }
+
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
 
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        rememberMe.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // we want to persist the decision to remember username and password,
+                // so we make a preference for whether or not the setting is switched
+                // on or off
+                SharedPreferences.Editor prefEdit = pref.edit();
+                prefEdit.putBoolean("remember", isChecked);
+                prefEdit.apply();
             }
         });
     }
